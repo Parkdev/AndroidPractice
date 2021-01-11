8ffbaf6e25ba1de72bdf9e060caa089af6a2782e
박지호 <devpark29@1thefull.com>
2021-01-11 11:12:21 +0900

    integrated first commit

---
+++ InventoryManagement/app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
@@ -0,0 +1,22 @@
+package com.example.inventorymanagement
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class AddProductActivity: Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.add_product, container, false)
+    }
+
+}
