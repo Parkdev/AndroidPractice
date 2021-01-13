8b64965cdc623fbf1da6feb19da78efe67e3cb44
박지호 <devpark29@1thefull.com>
2021-01-13 16:04:43 +0900

    test if

--- SayHello/app/src/main/java/com/example/sayhello/MainActivity.kt
+++ SayHello/app/src/main/java/com/example/sayhello/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.sayhello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sayhello.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_main)
+
+        binding.textSay.text = "bindingTest"
+        binding.btnSay.text = "Hello"
+        binding.btnSay.setOnClickListener {
+            if (binding.textSay.text == "bindingTest") {
+                binding.textSay.text = "click1"
+            } else {
+                binding.textSay.text = "bindingTest"
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
