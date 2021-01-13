88765ad8bd75c7a834ef25d3a976c762bd14432d
박지호 <devpark29@1thefull.com>
2021-01-13 16:38:44 +0900

    test when

--- MyBasicSyntax/app/src/main/java/com/example/mybasicsyntax/MainActivity.kt
+++ MyBasicSyntax/app/src/main/java/com/example/mybasicsyntax/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.mybasicsyntax
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mybasicsyntax.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        var now = 8
+        when (now) {
+            9,10 -> {
+                binding.mainText.text = "1"
+            }
+            in 11..20 -> {
+                binding.mainText.text = "2"
+            }
+            else -> {
+                binding.mainText.text = "3"
+            }
+        }
+        setContentView(binding.root)
     }
 }
