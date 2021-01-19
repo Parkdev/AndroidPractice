ac71bfe081367ad6f918ae57500162f77731c99c
박지호 <devpark29@1thefull.com>
2021-01-19 16:55:58 +0900

    preference practice

--- SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.kt
+++ SharedPreferences/app/src/main/java/com/example/sharedpreferences/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.PreferenceManager
+import com.example.sharedpreferences.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val shared = PreferenceManager.getDefaultSharedPreferences(this)
+        val checkboxvalue = shared.getBoolean("key_add_shortcut", false)
+        val switchValue = shared.getBoolean("key_switch_on", false)
+        val name = shared.getString("key_edit_name", "")
+        val selected = shared.getString("key_set_item","")
+
+        Log.d("CheckBoxValue", checkboxvalue.toString())
+        Log.d("SwitchValue", switchValue.toString())
+        Log.d("Name", name.toString())
+        Log.d("Selected",selected.toString())
     }
 }
