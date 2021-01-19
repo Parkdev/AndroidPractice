1ec86031a3c8dc7275716b4f2109ccb367a7ab42
박지호 <devpark29@1thefull.com>
2021-01-19 16:04:52 +0900

    file in/out practice

--- Permission/app/src/main/java/com/example/permission/MainActivity.kt
+++ Permission/app/src/main/java/com/example/permission/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.example.permission
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.permission.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCamera.setOnClickListener {
+            checkPermission()
+        }
     }
+
+    fun checkPermission() {
+        val cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED) {
+            startProcess()
+        } else {
+            requestPermission()
+        }
+    }
+
+    fun startProcess() {
+        Toast.makeText(this, "카페라를 실행합니다.", Toast.LENGTH_LONG).show()
+    }
+
+    fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 99)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            99 -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startProcess()
+                } else {
+                    finish()
+                }
+            }
+        }
+    }
+
+
 }
